d85439155fea19b9de8836fd8311fcf21b91faa1
anmfan <mizev.ivan04@mail.ru>
2025-05-14 21:58:56 +0500

    more fixes ip

--- diary/frontend/src/const.ts
+++ diary/frontend/src/const.ts
@@ -1,4 +1,4 @@
-export const AXIOS_BASE_URL = "http://localhost:5000/";
+export const AXIOS_BASE_URL = "http://45.130.9.13/";
 export const AXIOS_TIMEOUT = 5000;
 
 export type Nullable<T> = T | null;
