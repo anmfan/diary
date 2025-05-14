3fddaea4bedf127b956b32e57d4864bdf8c66295
anmfan <mizev.ivan04@mail.ru>
2025-05-14 21:56:25 +0500

    fixes ip

--- diary/backend/index.js
+++ diary/backend/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cookieParser())
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'http://45.130.9.13/',
     credentials: true,
 }))
 app.use(express.json());
