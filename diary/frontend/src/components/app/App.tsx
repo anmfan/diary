fc04c3d2fa080f4e546e11246a0a0cde78cc1d43
anmfan <mizev.ivan04@mail.ru>
2025-05-14 22:01:49 +0500

    more fix

--- diary/frontend/src/components/app/App.tsx
+++ diary/frontend/src/components/app/App.tsx
@@ -1,8 +1,13 @@
 import Routes from '../routes/Routes.tsx';
 import useAuthCheck from "@/hooks/useAuthCheck.ts";
+import {useEffect} from "react";
+import {getAllTeachers} from "@/redux/thunks/teachers-thunk.ts";
 
 const App = () => {
     useAuthCheck();
+    useEffect(() => {
+        getAllTeachers()
+    }, []);
     return (
         <Routes />
     );
