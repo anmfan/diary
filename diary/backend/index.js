7ee2254d7473f03dada869a0a1cdbf6a469661ec
anmfan <mizev.ivan04@mail.ru>
2025-05-22 00:00:20 +0500

    more fix

--- diary/backend/index.js
+++ diary/backend/index.js
@@ -18,8 +18,7 @@ app.use(cors({
 }))
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'static/avatars')));
-app.use(fileUpload({}))
-app.use('/api', router);
+app.use('/', router);
 
 app.use(express.static(path.resolve(__dirname, '../frontend/dist')));
 
