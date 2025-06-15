75b8ecadc2a0ef1376cb7e0f135fdb2a9f9624e0
anmfan <mizev.ivan04@mail.ru>
2025-06-15 13:10:38 +0500

    add to const variables

--- diary/frontend/src/const.ts
+++ diary/frontend/src/const.ts
@@ -1,5 +1,16 @@
 export const AXIOS_BASE_URL = 'http://45.130.9.13/api';
 export const AXIOS_TIMEOUT = 5000;
 
+export const lessonTimes = [
+    '08:00 – 9:30',
+    '9:40 – 11:10',
+    '12:00 – 13:30',
+    '13:40 – 15:10',
+    '15:50 – 17:20',
+    '17:30 – 19:00'
+];
+
+export const days = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт'];
+
 export type Nullable<T> = T | null;
 export const SERVER_BASE_URL = 'http://localhost:5000'
